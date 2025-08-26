5a4d137a2be67d497012ad85e1d527c552c5f95d
MasterOfCubesAU <masterofcubesau@gmail.com>
2025-08-26 14:12:56 +1000

    feat: add progress count

--- run.sh
+++ run.sh
@@ -26,24 +26,38 @@ fi
 # Establish what format to convert the files to
 read -p "What file format would you like to convert to? " format
 
+total_files=$(find "$INPUT_DIR" -name "*$FILE_SUFFIX"| wc -l)
+count=1
+
 # Find all input files and convert
 for filepath in $(find "$INPUT_DIR" -name "*$FILE_SUFFIX"); do
     filename=$(basename "$filepath")
     if [ -z "$*" ]; then
-        echo -en "Converting ${CYAN}$filename${NORMAL} to ${CYAN}${filename%.vol}_bm.$format${NORMAL}..."
-        "$BIN" -i "$filepath" -o "${OUTPUT_DIR}/${filename%.vol}_bm.$format" -r 0.2 0.2 0.2 >/dev/null
-        if [ "$?" -eq 0 ]; then
-        echo -e "${GREEN}DONE${NORMAL}"
-        else
-            echo -e "${RED}FAILED${NORMAL}"
-        fi
-        echo -en "Converting ${CYAN}$filename${NORMAL} to ${CYAN}${filename%.vol}_pd.$format${NORMAL}..."
-        "$BIN" -i "$filepath" -o "${OUTPUT_DIR}/${filename%.vol}_pd.$format" -r 0.2 0.2 0.2 -d >/dev/null
-         if [ "$?" -eq 0 ]; then
-        echo -e "${GREEN}DONE${NORMAL}"
-        else
-            echo -e "${RED}FAILED${NORMAL}"
-        fi
+        echo -en "(${count}/${total_files}) Converting ${CYAN}$filename${NORMAL} to ${CYAN}${filename%.vol}_bm.$format${NORMAL}..."
+        if [ -f "${OUTPUT_DIR}/${filename%.vol}_bm.$format" ]; then
+		echo -e "${NORMAL}SKIPPED${NORMAL}"
+	else
+		"$BIN" -i "$filepath" -o "${OUTPUT_DIR}/${filename%.vol}_bm.$format" -r 0.2 0.2 0.2 >/dev/null
+		if [ "$?" -eq 0 ]; then
+			echo -e "${GREEN}DONE${NORMAL}"
+		else
+		    echo -e "${RED}FAILED${NORMAL}"
+		fi
+    	fi
+        
+        
+        echo -en "(${count}/${total_files}) Converting ${CYAN}$filename${NORMAL} to ${CYAN}${filename%.vol}_pd.$format${NORMAL}..."
+         if [ -f "${OUTPUT_DIR}/${filename%.vol}_pd.$format" ]; then
+		echo -e "${NORMAL}SKIPPED${NORMAL}"
+	else
+		"$BIN" -i "$filepath" -o "${OUTPUT_DIR}/${filename%.vol}_pd.$format" -r 0.2 0.2 0.2 -d >/dev/null
+		if [ "$?" -eq 0 ]; then
+			echo -e "${GREEN}DONE${NORMAL}"
+		else
+			echo -e "${RED}FAILED${NORMAL}"
+		fi
+	fi
+	((count++))
     else
         "$BIN" -i "$filepath" -o "${OUTPUT_DIR}/${filename%.vol}.$format" "$@" >/dev/null
     fi
